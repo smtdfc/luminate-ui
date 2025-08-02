20c8db7b021bb6dc0b0d927992218a91ccf876e4
smtdfc <smtdfc.master@gmail.com>
2025-08-02 10:28:28 +0700

    feat(core): Add Accordion class

---
+++ packages/core/rollup.config.js
@@ -0,0 +1,21 @@
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import os from 'os';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: './dist/index.js',
+    format: 'esm',
+    sourcemap: !isProduction,
+  },
+  plugins: [
+    typescript({ tsconfig: './tsconfig.json' }),
+    isProduction &&
+      terser({
+        maxWorkers: os.cpus().length || 1,
+      }),
+  ],
+};
---
+++ packages/core/src/components/accordion.ts
@@ -0,0 +1,25 @@
+export class Accordion{
+  public header:HTMLElement;
+  constructor(public element:HTMLElement){
+    this.header = element.querySelector(".accordion-header") ?? document.createElement("span");
+    this.init()
+  }
+  
+  private init(){
+    this.header.addEventListener("click",()=> this.toggle());
+  }
+  
+  open(){
+    this.element.classList.add("active");
+  }  
+  
+  close(){
+    this.element.classList.remove("active");
+  }
+  
+  toggle(){
+    if(this.element.classList.contains("active")) this.close();
+    else this.open();
+  }
+
+}
